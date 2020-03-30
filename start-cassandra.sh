cccb09fcbdb458a99761922ae241845f7a0c1f7c
Alex K <akotov@gmail.com>
2020-03-29 18:42:16 -0700

    started shell script

---
+++ start-cassandra.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# instance DC
+INSTANCE_DC=DC1
+
+# instance cluster name, default "INSTANCE_DC Cluster"
+CLUSER_NAME="${INSTANCE_DC} Cluster"
+
+# instance CPU count
+INSTANCE_CPU_COUNT=2
+
+# instance RAM in GB
+INSTANCE_RAM_GB=4
+
+# instance disk size in GB
+INSTANCE_DISK_SIZE_GB=20
+
+# instance count
+INSTANCE_COUNT=3
+
+
